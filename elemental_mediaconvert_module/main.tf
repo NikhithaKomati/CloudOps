c537fd536fcb1aed391f302de6c672cc51fa8245
NikhithaKomati <nikhithakomati1234@gmail.com>
2024-06-13 20:40:22 +0530

    Update main.tf

--- elemental_mediaconvert_module/main.tf
+++ elemental_mediaconvert_module/main.tf
@@ -13,28 +13,28 @@ resource "aws_iam_role" "mediaconvert_role" {
         "Principal" : {
           "Service" : "mediaconvert.amazonaws.com"
         },
-        "Action" : "sts:AssumeRole"  
+        "Action" : "sts:AssumeRole"
       }
     ]
   })
 }
 
 
-# Create an IAM policy for Kinesis Data Firehose
+# Create an IAM policy for mediaconvert
 resource "aws_iam_policy" "mediaconvert_policy" {
-  name        = "mediaconvert-policy"
-  description = "IAM policy for elemental mediaconvert"
-  
+  name        = var.iam_policy
+  description = "IAM policy for Kinesis Data Firehose"
+
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": [
-          "mediaconvert:PutRecord",
-          "mediaconvert:PutRecordBatch"
+        "Effect" : "Allow",
+        "Action" : [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch"
         ],
-        "Resource": "*"
+        "Resource" : "*"
       }
     ]
   })
@@ -43,17 +43,16 @@ resource "aws_iam_policy" "mediaconvert_policy" {
 
 # Attach the IAM policy to the IAM role
 resource "aws_iam_policy_attachment" "mediaconvert_attachment" {
-  name       = "mediaconvert-attachment"
+  name       = var.iam_attchment_name
   policy_arn = aws_iam_policy.mediaconvert_policy.arn
   roles      = [aws_iam_role.mediaconvert_role.name]
 }
 
 
-
 resource "aws_media_convert_queue" "example_queue" {
-  name = var.media_convert_queue_name
+  name   = var.media_convert_queue_name
   status = "ACTIVE"
- tags = {
+  tags = {
     Name         = var.tag_name
     team         = var.team
     env          = var.env
